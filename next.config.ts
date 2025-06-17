a4251375a90be1558165dcfa10389c677a450e1a
Marcella Paiva <santosmarcella.1212@gmail.com>
2025-06-17 19:14:07 +0000

    studio-3893248103:~/studio{master}$ npm run dev
    > nextn@0.1.0 dev
    > nex

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,15 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://9003-firebase-studio-1749839699180.cluster-vpxjqdstfzgs6qeiaf7rdlsqrc.cloudworkstations.dev',
+      // You might need to add other variations if the port or subdomain changes,
+      // or if you access it via other proxied URLs.
+      // For example, if you sometimes access it via http:
+      'http://9003-firebase-studio-1749839699180.cluster-vpxjqdstfzgs6qeiaf7rdlsqrc.cloudworkstations.dev'
+    ],
+  },
 };
 
 export default nextConfig;
